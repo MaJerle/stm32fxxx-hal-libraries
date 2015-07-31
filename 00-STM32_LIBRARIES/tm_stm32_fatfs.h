e86309b0bdbf304113b4c02513398955fb46c6dc
Tilen Majerle <tilen.majerle@gmail.com>
2015-07-31 08:44:07 +0200

    Comments updated

--- 00-STM32_LIBRARIES/tm_stm32_fatfs.h
+++ 00-STM32_LIBRARIES/tm_stm32_fatfs.h
@@ -65,7 +65,7 @@
 |                   |   1   |CD/DAT3  PC11   -      Connector data line 3 |CS     PB5         Chip select for SPI 
 |                   |   2   |CMD      PD2    PD2    Command/Response line |MOSI   PA7         Data input for SPI 
 |                   |   3   |VSS1     GND    GND    GND                   |VSS1   GND         GND 
-|   SD CARD Pinout  |   4	  |VDD      3.3V   3.3V   3.3V Power supply     |VDD    3.3V        3.3V Power supply 
+|   SD CARD Pinout  |   4   |VDD      3.3V   3.3V   3.3V Power supply     |VDD    3.3V        3.3V Power supply 
 |                   |   5   |CLK      PC12   PC12   Clock                 |SCK    PA5         Clock for SPI 
 |                   |   6   |VSS2     GND    GND    GND                   |VSS2   GND         GND 
 |                   |   7   |DAT0     PC8    PC8    Connector data line 0 |MISO   PA6         Data output for SPI 
